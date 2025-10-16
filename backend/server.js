b7530bb22a41ea4e0e58c9fde5cb078ecc9a538a
Srinivas J Sutar <154606190+srinivasjsutar@users.noreply.github.com>
2025-10-16 18:05:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI) // Removed deprecated options
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
